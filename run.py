dbec896c2dd15d185f5656046fa5b87881a25f9c
glacey <garretrlacey@gmail.com>
2023-09-22 10:59:56 +0100

    add tos

--- run.py
+++ run.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/tos')
+def index():
+    return render_template('tos.html')
+
+
+@app.route('/privacy')
+def index():
+    return render_template('privacy.html')
